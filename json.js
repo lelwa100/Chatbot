3b70a145b48d85a803238ac0d81de7e19e24d9c0
Xolelwa <xolelwacekiso@gmail.com>
2025-08-20 10:18:11 +0200

    made changes

--- json.js
+++ json.js
@@ -6,7 +6,11 @@ function talk() {
       "what can i do for you": "Please give us a follow & like ❤️",
       "your followers": "I have my family of 5000 members. I don't have followers, I have a supportive family 💪",
       "ok": "Thank you so much 🙏",
-      "bye": "Okay! We'll meet soon 👋"
+      "bye": "Okay! We'll meet soon 👋",
+      "where do you live": "Berea📍",
+      "goodnight" : "goodnight sweeet dreams😴",
+      "what is oop": "OOP means Object-Oriented Programming. It uses classes & objects 🖥️",
+      "i love you": "Aww ❤️ Love you too!",
     };
   
     var user = document.getElementById("userBox").value.trim().toLowerCase();
